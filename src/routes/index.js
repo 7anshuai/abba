c0996463cc4c8157959e31cf7908e40773c0d4e5
7anshuai <7anshuai@gmail.com>
2018-06-20 17:02:14 +0800

    Set helper functions for views

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,22 @@ import Variant from '../models/Variant';
 const routes = Router();
 
 /**
+ * Set helper methods
+ */
+routes.use(function(req, res, next) {
+  res.locals.setTitle = function(value = null) {
+    let title;
+    if (value)
+      title = value;
+    return title ? `Abba - ${title}` : 'Abba';
+  }
+  res.locals.selected = function(value, present) {
+    if (value === present) return ' selected '
+  }
+  next();
+});
+
+/**
  * Redirect to /admin/experiments
  */
 routes.get('/', (req, res) => {
@@ -33,7 +49,11 @@ routes.get('/admin/experiments', (req, res, next) => {
       Promise
         .all(variants)
         .then(variants => {
-          res.render('experiments', { title: 'Abba - Experiments',  host: req.get('host'), experiments, variants });
+          experiments = experiments.map((experiment, index) => {
+            experiment.variants = variants[index];
+            return experiment;
+          });
+          res.render('experiments', { title: 'Experiments',  host: req.get('host'), experiments });
         });
     })
     .catch(err => {
